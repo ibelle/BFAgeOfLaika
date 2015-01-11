36da4c213faa8a0dd9dc2d96a674fb8f63e1ff71
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-10 20:58:14 -0500

    Age Of Laika Challenge 2
    Age Of Laika Challenge 2

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -44,6 +44,37 @@ class ViewController: UIViewController {
         humanYearsTextField.resignFirstResponder()
         
     }
+    @IBAction func convertToRealDogYears(sender: UIButton) {
+        
+        /*
+        Change the conversion method (which evaluates when your UIButton is touched) to use if statements. The real conversion for dog years is as follows; Each human year equals 10.5 dog years for the first two years, and then 4 dog years for each human year after. Sounds like a word problem? Well, it is. You will find yourself frequently taking what is asked of you, and translating that into code in your project. Again, best of luck!
+        */
+        if  let humanYearsFromTextField = (humanYearsTextField.text as NSString?)?.doubleValue
+        {
+            if(humanYearsFromTextField > 0){
+                let youngConversionFactor = 10.5
+                let YOUNG_DOG_FACTOR = 2.0
+                let oldConversionFactor = 4.0
+                var dogYears:Double?
+                
+                if ( humanYearsFromTextField <= YOUNG_DOG_FACTOR){
+                    dogYears = youngConversionFactor * humanYearsFromTextField
+                }else{
+                  dogYears = (youngConversionFactor * YOUNG_DOG_FACTOR) + oldConversionFactor * (humanYearsFromTextField - YOUNG_DOG_FACTOR)
+                }
+                if dogYears != nil {
+                    dogYearsLabel.text = "\(dogYears!) in Dog years"
+                    dogYearsLabel.hidden = false
+                }
+            }else{
+                clearConversionLabel()
+            }
+        }else{
+            clearConversionLabel()
+        }
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+    }
     
     func clearConversionLabel() {
         dogYearsLabel.text = ""
